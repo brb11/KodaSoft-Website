0266a20f3a4c25547b27f1d3c6d2140e49f9c267
Ebrahim Basha <ebrahim4comp@gmail.com>
2026-07-25 10:11:59 +0300

    Update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/kodasoft-website/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
